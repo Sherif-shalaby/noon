57eec274313dd926feefd4bdf5b6367e357825a6
Raneem-ayman <73843554+Raneem-ayman@users.noreply.github.com>
2023-07-09 10:05:06 +0300

    Authentication & roles and permissions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,9 +14,39 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
-    return view('home.index');
+    if (Auth::check()) {
+        return view('home.index');
+    } else {
+        return redirect('/login');
+    }
+});
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::resource('class', 'ClassController');
+    Route::resource('category', 'CategoryController');
+    Route::resource('product', 'ProductController');
+    Route::resource('brand', 'BrandController');
+    Route::resource('currency', 'CurrencyController');
+    Route::resource('store', 'StoreController');
+    Route::resource('productstore', 'ProductStoreController');
+    Route::resource('customer', 'CustomerController');
+    Route::resource('customertype', 'CustomerTypeController');
+    Route::resource('customerbalanceadjustments', 'CustomerBalanceAdjustmentsController');
+    Route::resource('employee', 'EmployeeController');
+    Route::resource('jobtype', 'JobTypeController');
+    Route::resource('exchangerates', 'ExchangeRatesController');
+    Route::resource('productdiscount', 'ProductDiscountController');
+
+    Route::post('/logout', function () {
+        Auth::logout();
+        return redirect('/login');
+    });
 });
 
-
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+// Route::get('/home', [HomeController::class, 'index'])->name('home');
