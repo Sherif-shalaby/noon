4e8bc01bf67828724caecce2b3c7b4e14a5e4677
doaabakhiet <doaabakhiet11@gmail.com>
2023-07-25 03:42:32 -0700

    product fix

--- app/Models/ProductPrice.php
+++ app/Models/ProductPrice.php
@@ -11,7 +11,7 @@ class ProductPrice extends Model
     public $timestamps = true;
 
     protected $dates = ['deleted_at'];
-    protected $fillable = array('product_id','price','price_start_date', 'price_end_date', 'price_customer_types', 'price_customers', 'price_category', 'is_price_permenant', 'created_by', 'updated_by', 'deleted_by');
+    protected $fillable = array('product_id','price_type','bonus_quantity','quantity','price','price_start_date', 'price_end_date', 'price_customer_types', 'price_customers', 'price_category', 'is_price_permenant', 'created_by', 'updated_by', 'deleted_by');
     // protected $visible = array('created_by');
     protected $casts =['price_customer_types'=>'array'];
     public function product()
