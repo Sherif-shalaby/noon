9d5625d4e8106dbf53e175bfb5c309bc114f4e7f
doaabakhiet <doaabakhiet11@gmail.com>
2023-09-26 23:42:51 -0700

    fix

--- resources/views/initial-balance/create.blade.php
+++ resources/views/initial-balance/create.blade.php
@@ -15,9 +15,7 @@
             </div>
             <div class="col-md-4 col-lg-4">
                 <div class="widgetbar">
-                    {{--                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createBrandModal">--}}
-                    {{--                        @lang('lang.add_stock')--}}
-                    {{--                    </button>--}}
+                    <a type="button" class="btn btn-primary" href="{{route('initial-balance.index')}}">@lang('lang.initial_balance')</a>
                 </div>
             </div>
         </div>
