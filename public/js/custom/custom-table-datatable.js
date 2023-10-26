f48673cd17c45616a8fd6a5a81ce24e33bc995e2
eitharSaeed <eithar.saeed256@gmail.com>
2023-10-26 10:29:19 +0300

    Update custom-table-datatable.js

--- public/js/custom/custom-table-datatable.js
+++ public/js/custom/custom-table-datatable.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         responsive: true
     });
     $('#default-datatable').DataTable( {
-        "order": [[ 3, "desc" ]],
+        "order": [],
         responsive: true,
     } );
     var table = $('#datatable-buttons').DataTable({
