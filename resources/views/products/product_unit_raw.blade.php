d87ad8e220246f29a15f91fe7fc20926f5bbdf98
eitharSaeed <eithar.saeed256@gmail.com>
2023-12-11 14:37:11 +0200

    update adding

--- resources/views/products/product_unit_raw.blade.php
+++ resources/views/products/product_unit_raw.blade.php
@@ -10,13 +10,13 @@
         <div class="col-md-2">
             {!! Form::label('unit', __('lang.small_filling'), ['class'=>'h5 pt-3']) !!}
             <div class="d-flex justify-content-center">
-                <select name="products[{{ $key }}][variations][{{$index}}][new_unit_id]"  data-name='unit_id' data-index="{{$index}}"  class="form-control unit_select select2 unit_id{{$index}}" style="width: 100px;" data-key="{{ $key }}">
+                <select name="products[{{ $key }}][variations][{{$index}}][new_unit_id]"  data-name='unit_id' data-index="{{$index}}"  class="form-control unit_select select2 unit_id{{$index}}{{$key ??''}}" style="width: 100px;" data-key="{{ $key }}">
                     <option value="">{{__('lang.please_select')}}</option>
                     @foreach($units as $unit)
                         <option @if($key == 0 && isset( $variation->unit_id) &&($variation->unit_id == $unit->id)) selected @endif  value="{{$unit->id}}">{{$unit->name}}</option>
                     @endforeach
                 </select>
-                <button type="button" class="btn btn-primary btn-sm ml-2 add_unit_raw" data-toggle="modal" data-index="{{$index}}" data-target=".add-unit" href="{{route('units.create')}}"><i class="fas fa-plus"></i></button>
+                <button type="button" class="btn btn-primary btn-sm ml-2 btn-add-modal add_unit_raw" data-key="{{ $key }}" data-toggle="modal" data-index="{{$index}}" data-target=".add-unit" href="{{route('units.create')}}"><i class="fas fa-plus"></i></button>
             </div>
         </div>
         <div class="col-md-2 pl-5">
