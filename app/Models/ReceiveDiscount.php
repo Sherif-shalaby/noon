c7f95c31012a504b9e020e86f0c8014d516da2d7
Raneem-ayman <73843554+Raneem-ayman@users.noreply.github.com>
2024-01-09 01:08:33 +0200

    update

--- app/Models/ReceiveDiscount.php
+++ app/Models/ReceiveDiscount.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReceiveDiscount extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = array('stock_transaction_id','discount_type','amount','amount_dollar','change','change_dollar','received_amount','received_amount_dollar','created_by');
 }
