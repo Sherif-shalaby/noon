715d22e05a9722cd0361bb9cf8d3456b132ae0ed
nour-alshikh <nooralshikh30@gmail.com>
2024-01-10 15:31:26 +0200

    stock bars

--- resources/views/livewire/add-stock/create.blade.php
+++ resources/views/livewire/add-stock/create.blade.php
@@ -322,87 +322,9 @@
                                 </div>
                             </div>
 
+
+
                             <div class="row @if (app()->isLocale('ar')) flex-row-reverse @else flex-row @endif">
-                                <div class="col-md-4 animate__animated animate__bounceInRight"
-                                    style="animation-delay: 1.7s">
-                                    <div class="search-box input-group">
-                                        <input type="search" name="search_by_product_symbol"
-                                            id="search_by_product_symbol"
-                                            wire:model.debounce.200ms="search_by_product_symbol"
-                                            placeholder="@lang('lang.enter_product_symbol')" class="form-control" autocomplete="off">
-
-                                        @if (!empty($search_result) && !empty($search_by_product_symbol))
-                                            <ul id="ui-id-1" tabindex="0"
-                                                class="ui-menu ui-widget ui-widget-content ui-autocomplete ui-front rounded-2"
-                                                style="top: 37.423px; left: 39.645px; width: 90.2%;">
-                                                @foreach ($search_result as $product)
-                                                    <li class="ui-menu-item"
-                                                        wire:click="add_product({{ $product->id }})">
-                                                        <div id="ui-id-73" tabindex="-1"
-                                                            class="ui-menu-item-wrapper">
-                                                            @if ($product->image)
-                                                                <img src="{{ asset('uploads/products/' . $product->image) }}"
-                                                                    alt="{{ $product->name }}" class="img-thumbnail"
-                                                                    width="100px">
-                                                            @else
-                                                                <img src="{{ asset('uploads/' . $settings['logo']) }}"
-                                                                    alt="{{ $product->name }}" class="img-thumbnail"
-                                                                    width="100px">
-                                                            @endif
-                                                            {{ $product->product_symbol ?? '' }} -
-                                                            {{ $product->name }}
-                                                        </div>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-md-8 mb-3 animate__animated animate__bounceInLeft"
-                                    style="animation-delay: 1.7s">
-                                    <div class="search-box input-group">
-                                        <button type="button" class="btn btn-secondary" id="search_button"><i
-                                                class="fa fa-search"></i>
-                                        </button>
-                                        <input type="search" name="search_product" id="search_product"
-                                            wire:model.debounce.200ms="searchProduct" placeholder="@lang('lang.enter_product_name_to_print_labels')"
-                                            class="form-control" autocomplete="off">
-                                        {{--                                    <button type="button" class="btn btn-success  btn-modal" --}}
-                                        {{--                                            data-href="{{ route('products.create') }}?quick_add=1" --}}
-                                        {{--                                            data-container=".view_modal"><i class="fa fa-plus"></i> --}}
-                                        {{--                                    </button> --}}
-                                        @if (!empty($search_result) && !empty($searchProduct))
-                                            <ul id="ui-id-1" tabindex="0"
-                                                class="ui-menu ui-widget ui-widget-content ui-autocomplete ui-front rounded-2"
-                                                style="top: 37.423px; left: 39.645px; width: 90.2%;">
-                                                @foreach ($search_result as $product)
-                                                    <li class="ui-menu-item"
-                                                        wire:click="add_product({{ $product->id }})">
-                                                        <div id="ui-id-73" tabindex="-1"
-                                                            class="ui-menu-item-wrapper">
-                                                            @if ($product->image)
-                                                                <img src="{{ asset('uploads/products/' . $product->image) }}"
-                                                                    alt="{{ $product->name }}" class="img-thumbnail"
-                                                                    width="100px">
-                                                            @else
-                                                                <img src="{{ asset('uploads/' . $settings['logo']) }}"
-                                                                    alt="{{ $product->name }}" class="img-thumbnail"
-                                                                    width="100px">
-                                                            @endif
-                                                            {{ $product->sku ?? '' }} - {{ $product->name }}
-                                                        </div>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                        {{--                                    {{$search_result->links()}} --}}
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div
-                                class="row @if (app()->isLocale('ar')) flex-row-reverse @else flex-row @endif">
                                 <div class="col-md-1 border border-1 p-0 animate__animated animate__bounceInRight"
                                     style="height: 100vh;overflow: scroll;animation-delay: 1.8s">
                                     <div class="p-3 text-center font-weight-bold " style="background-color: #eee;">
@@ -541,7 +463,85 @@
 
 
                                     @endif
+                                    <div class="row @if (app()->isLocale('ar')) flex-row-reverse @else flex-row @endif"
+                                        style="position: relative;z-index: 9999;">
+                                        <div class="col-md-4 animate__animated animate__bounceInRight">
+                                            <div class="search-box input-group">
+                                                <input type="search" name="search_by_product_symbol"
+                                                    id="search_by_product_symbol"
+                                                    wire:model.debounce.200ms="search_by_product_symbol"
+                                                    placeholder="@lang('lang.enter_product_symbol')" class="form-control"
+                                                    autocomplete="off">
 
+                                                @if (!empty($search_result) && !empty($search_by_product_symbol))
+                                                    <ul id="ui-id-1" tabindex="0"
+                                                        class="ui-menu ui-widget ui-widget-content ui-autocomplete ui-front rounded-2"
+                                                        style="top: 37.423px; left: 39.645px; width: 90.2%;">
+                                                        @foreach ($search_result as $product)
+                                                            <li class="ui-menu-item"
+                                                                wire:click="add_product({{ $product->id }})">
+                                                                <div id="ui-id-73" tabindex="-1"
+                                                                    class="ui-menu-item-wrapper">
+                                                                    @if ($product->image)
+                                                                        <img src="{{ asset('uploads/products/' . $product->image) }}"
+                                                                            alt="{{ $product->name }}"
+                                                                            class="img-thumbnail" width="100px">
+                                                                    @else
+                                                                        <img src="{{ asset('uploads/' . $settings['logo']) }}"
+                                                                            alt="{{ $product->name }}"
+                                                                            class="img-thumbnail" width="100px">
+                                                                    @endif
+                                                                    {{ $product->product_symbol ?? '' }} -
+                                                                    {{ $product->name }}
+                                                                </div>
+                                                            </li>
+                                                        @endforeach
+                                                    </ul>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 mb-3 animate__animated animate__bounceInLeft">
+                                            <div class="search-box input-group">
+                                                <button type="button" class="btn btn-secondary"
+                                                    id="search_button"><i class="fa fa-search"></i>
+                                                </button>
+                                                <input type="search" name="search_product" id="search_product"
+                                                    wire:model.debounce.200ms="searchProduct"
+                                                    placeholder="@lang('lang.enter_product_name_to_print_labels')" class="form-control"
+                                                    autocomplete="off">
+                                                {{--                                    <button type="button" class="btn btn-success  btn-modal" --}}
+                                                {{--                                            data-href="{{ route('products.create') }}?quick_add=1" --}}
+                                                {{--                                            data-container=".view_modal"><i class="fa fa-plus"></i> --}}
+                                                {{--                                    </button> --}}
+                                                @if (!empty($search_result) && !empty($searchProduct))
+                                                    <ul id="ui-id-1" tabindex="0"
+                                                        class="ui-menu ui-widget ui-widget-content ui-autocomplete ui-front rounded-2"
+                                                        style="top: 37.423px; left: 39.645px; width: 90.2%;">
+                                                        @foreach ($search_result as $product)
+                                                            <li class="ui-menu-item"
+                                                                wire:click="add_product({{ $product->id }})">
+                                                                <div id="ui-id-73" tabindex="-1"
+                                                                    class="ui-menu-item-wrapper">
+                                                                    @if ($product->image)
+                                                                        <img src="{{ asset('uploads/products/' . $product->image) }}"
+                                                                            alt="{{ $product->name }}"
+                                                                            class="img-thumbnail" width="100px">
+                                                                    @else
+                                                                        <img src="{{ asset('uploads/' . $settings['logo']) }}"
+                                                                            alt="{{ $product->name }}"
+                                                                            class="img-thumbnail" width="100px">
+                                                                    @endif
+                                                                    {{ $product->sku ?? '' }} - {{ $product->name }}
+                                                                </div>
+                                                            </li>
+                                                        @endforeach
+                                                    </ul>
+                                                @endif
+                                                {{--                                    {{$search_result->links()}} --}}
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
