69cba3addaede474b8baf72066cb3bc228d07cb1
eitharSaeed <eithar.saeed256@gmail.com>
2024-02-27 11:59:25 +0200

    update

--- database/migrations/2024_01_13_131426_modify_your_table_foreign_key_column_nullable.php
+++ database/migrations/2024_01_13_131426_modify_your_table_foreign_key_column_nullable.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('cash_register_transactions', function (Blueprint $table) {
-//            $table->unsignedBigInteger('transaction_id')->nullable()->change();
-            $table->unsignedBigInteger('transaction_id')->nullable();
+            $table->unsignedBigInteger('transaction_id')->nullable()->change();
+//            $table->unsignedBigInteger('transaction_id')->nullable();
         });
     }
 
--- database/migrations/2024_01_18_155315_edit_columns_to_transaction_sell_lines_table.php
+++ database/migrations/2024_01_18_155315_edit_columns_to_transaction_sell_lines_table.php
@@ -13,9 +13,23 @@ return new class extends Migration
      */
     public function up()
     {
+
+        // Drop foreign key constraints if they exist
         Schema::table('transaction_sell_lines', function (Blueprint $table) {
-            $table->unsignedInteger('customer_id')->nullable()->change() ;
-            $table->unsignedInteger('supplier_id')->nullable()->change() ;
+            $table->dropForeign('transaction_sell_lines_customer_id_foreign');
+            $table->dropForeign('transaction_sell_lines_supplier_id_foreign');
+        });
+
+        // Modify the columns
+        Schema::table('transaction_sell_lines', function (Blueprint $table) {
+            $table->unsignedBigInteger('customer_id')->nullable()->change();
+            $table->unsignedBigInteger('supplier_id')->nullable()->change();
+        });
+
+        // Recreate foreign key constraints
+        Schema::table('transaction_sell_lines', function (Blueprint $table) {
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
         });
     }
 
