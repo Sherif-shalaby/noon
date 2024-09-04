54857680a29569313f174a120bb96886b1fca5ac
nour-alshikh <nooralshikh30@gmail.com>
2024-09-04 18:21:05 +0300

    invoice fix

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('title', __('site.Sale_Screen'))
 @push('css')
-    <link rel="stylesheet" href="{{ asset('salescreen/css/main.css') }}" />
-    <style>
-        .customer_drop_down .select2-container {
-            width: 75% !important
-        }
+<link rel="stylesheet" href="{{ asset('salescreen/css/main.css') }}" />
+<style>
+    .customer_drop_down .select2-container {
+        width: 75% !important
+    }
 
-        /* .form-select {
+    /* .form-select {
                 height: 100%;
                 padding-bottom: 0;
                 padding-top: 0;
@@ -24,14 +24,18 @@
                 box-shadow: 0 0 0 0 !important;
                 background-color: white !important;
             } */
-    </style>
+
+    .select2-container--open .select2-dropdown {
+        left: -70px !important
+    }
+</style>
 @endpush
 
 @section('content')
-    @livewire('invoices.create')
+@livewire('invoices.create')
 @endsection
 @push('js')
-    <script src="{{ asset('salescreen/js/main.js') }}"></script>
-    <script src="{{ asset('salescreen/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('salescreen/js/all.min.js') }}"></script>
+<script src="{{ asset('salescreen/js/main.js') }}"></script>
+<script src="{{ asset('salescreen/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('salescreen/js/all.min.js') }}"></script>
 @endpush
